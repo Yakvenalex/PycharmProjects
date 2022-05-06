ef6b05e2d310db76cfb50ca39b1fe926a376da34
Yakovenko Oleksii <mr.mnogo@gmail.com>
2022-05-07 02:51:36 +0300

    исправил ошибку с пикселями в хтмл ффайле

--- my_script/parsing_web_site_progect/actris/draft.py
+++ my_script/parsing_web_site_progect/actris/draft.py
@@ -1,11 +1,10 @@
 import csv
 
+
 def get_csv():
     with open('tetyana.csv', 'a', newline='', encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow(['имя', 'возраст', 'рейтинг', 'ссылка'])
 
+
 get_csv()
-
-
-
