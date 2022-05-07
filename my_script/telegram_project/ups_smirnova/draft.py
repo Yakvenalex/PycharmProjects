eccbced98abe4b4b6f89e8ac4ab57b3b91f2e36f
Yakovenko Oleksii <mr.mnogo@gmail.com>
2022-05-07 22:25:54 +0300

    Написал програмку с приветствиями

--- my_script/telegram_project/ups_smirnova/draft.py
+++ my_script/telegram_project/ups_smirnova/draft.py
@@ -1,3 +1,9 @@
 import random
+l = []
 
-print(random.randint(100, 300))
+with open('hello_list.txt', 'r', newline='', encoding="utf-8") as file:
+    for i in file:
+        l.append(i.strip())
+
+random_index = random.randint(0, len(l) - 1)
+print(l[random_index])
