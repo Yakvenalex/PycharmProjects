75a8e2c0fb312d53e96681004ba4f8e82a25461f
Yakovenko Oleksii <mr.mnogo@gmail.com>
2022-05-07 03:36:46 +0300

    удалил тестовые функции

--- my_script/telegram_project/telegram_download_media/config.py
+++ my_script/telegram_project/telegram_download_media/config.py
@@ -1,18 +1,3 @@
 api_id = 15214908
 api_hash = '2eba7357a141a63b2acf200eafd3bb5b'
 plugins = dict(root="plugins")
-
-
-def plus(a,b):
-    return a+b
-
-
-print(plus(5,12))
-
-
-def minus(a,b):
-    return a-b
-
-print(minus(10,3))
-
-
