cb6c3d67c9f4825ba5cc977f9493f5ffe77323ef
Yakovenko Oleksii <mr.mnogo@gmail.com>
2022-05-07 13:00:54 +0300

    добавил управление эмитацией набора текста.

--- my_script/Lerning_file/Pyrogram/new_lern.py
+++ my_script/Lerning_file/Pyrogram/new_lern.py
@@ -2,10 +2,21 @@ import time
 from pyrogram import Client, filters, enums
 from apscheduler.schedulers.background import BackgroundScheduler
 from config import *
+import asyncio
 
 app = Client('account',
              api_id,
              api_hash)
+my_text = 'Как бы я хотел сделать: отправляется действие, потом как оно заканчивается, отправляется файл. Но получается так, что действие длится 5 секунд, а потом еще 5 секунд занимает отправка файла, и это время пользователь не понимает, это бот завис или файл еще отправляется. Как увеличить время действия до непосредственной отправки файла?'
 
-with app:
-    app.send_message(5332290640, 'тест')
+
+async def main():
+    async with app:
+        for _ in range(3):
+            await app.send_chat_action(5332290640, enums.ChatAction.TYPING)
+            await asyncio.sleep(5)
+        await asyncio.sleep(5)
+        await app.send_message(5332290640, my_text)
+
+
+app.run(main())
