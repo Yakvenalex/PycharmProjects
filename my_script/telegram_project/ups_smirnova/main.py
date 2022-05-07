32c105e2c687bd345a48fdfb5d87e53481d99418
Yakovenko Oleksii <mr.mnogo@gmail.com>
2022-05-07 18:06:54 +0300

    зашел снова

--- my_script/telegram_project/ups_smirnova/main.py
+++ my_script/telegram_project/ups_smirnova/main.py
@@ -4,4 +4,4 @@ from config import *
 app = Client('account', api_id, api_hash)
 
 with app:
-    app.send_message('yakvenalexx', 'Привет!!!')
+    app.send_message('AUTSA', 'Это сообщение сгенерировал наш бот Юлия Смирнова)')
