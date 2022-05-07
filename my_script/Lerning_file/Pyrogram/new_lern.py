ccc43d92169d32973099ecc2d49d9a8d0c22e2af
Yakovenko Oleksii <mr.mnogo@gmail.com>
2022-05-07 13:57:48 +0300

    прописал поисковик по сообщениям в чате

--- my_script/Lerning_file/Pyrogram/new_lern.py
+++ my_script/Lerning_file/Pyrogram/new_lern.py
@@ -10,12 +10,19 @@ app = Client('account',
 my_text = 'Как бы я хотел сделать: отправляется действие, потом как оно заканчивается, отправляется файл. Но получается так, что действие длится 5 секунд, а потом еще 5 секунд занимает отправка файла, и это время пользователь не понимает, это бот завис или файл еще отправляется. Как увеличить время действия до непосредственной отправки файла?'
 
 
-async def main():
-    async with app:
-        for _ in range(3):
-            await app.send_chat_action(5332290640, enums.ChatAction.TYPING)
-            await asyncio.sleep(5)
-        await asyncio.sleep(5)
-        await app.send_message(5332290640, my_text)
+# async def main():
+#     async with app:
+#         for _ in range(3):
+#             await app.send_chat_action(5332290640, enums.ChatAction.TYPING)
+#             await asyncio.sleep(5)
+#         await asyncio.sleep(5)
+#         await app.send_message(5332290640, my_text)
 
-app.run(main())
+# async def main():
+#     async with app:
+#         async for message in app.search_messages('1242606918', query="люблю"):
+#             if message.from_user.id == 1242606918:
+#                 print(f'{message.text} - написала любимая')
+#             else:
+#                 print(f'{message.text} - написал я')
+# app.run(main())
